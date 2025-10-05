fffb39b544906338540f8ade37f919f70f801906
Renato Carrasco <renatocarrasco10@gmail.com>
2025-10-05 19:26:54 -0300

    Estrutura Inicial

---
+++ AcademiaDev/src/AcademiaDev.GestaoConteudo.Data/GestaoConteudoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaDev.GestaoConteudo.Data
+{
+    internal class GestaoConteudoContext: DbContext
+
+    {
+    }
+}
